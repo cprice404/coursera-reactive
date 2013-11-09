99f3f649a8ebfea6e6845a30c8f15a9e5ecbb92d
Chris Price <chris@puppetlabs.com>
2013-11-09 14:02:41 -0800

    Finished, tests passing

--- week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -11,6 +11,14 @@ import scala.annotation.tailrec
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
+  def popAll(h: H) : List[A] = {
+    @tailrec
+    def popAcc(as : List[A], h: H) : List[A] = {
+      if (isEmpty(h)) as else popAcc(as :+ findMin(h), deleteMin(h))
+    }
+    popAcc(List[A](), h)
+  }
+
   property("min1") = forAll { a: A =>
     val h = insert(a, empty)
     findMin(h) == a
@@ -28,25 +36,34 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   }
 
   property("sorted") = forAll { h: H =>
-    def popAll(h: H) : List[A] = {
-      @tailrec
-      def popAcc(as : List[A], h: H) : List[A] = {
-        if (isEmpty(h)) as
-        else {
-          val a = findMin(h)
-          popAcc(as :+ a, deleteMin(h))
-        }
-      }
-      popAcc(List[A](), h)
-    }
-
     val as:List[A] = popAll(h)
     as == as.sorted
   }
 
+  property("minMeld") = forAll { (h1: H, h2: H) =>
+    (! (isEmpty(h1) || isEmpty(h2))) ==> {
+      val min1 = findMin(h1)
+      val min2 = findMin(h2)
+      val meldMin = findMin(meld(h1,h2))
+      meldMin == min(min1, min2)
+    }
+  }
+
+  property("sortedAndAssocMeld") = forAll { (h1: H, h2: H) =>
+    val as1:List[A] = popAll(meld(h1, h2))
+    val as2:List[A] = popAll(meld(h2, h1))
+    (as1 == as1.sorted) && (as1 == as2)
+  }
+
+  property("insDeleteMin") = forAll(nonEmptyHeaps, arbitrary[A]) { (h: H, a: A) =>
+    val as1 = popAll(h)
+    val as2 = popAll(deleteMin(insert(findMin(h), h)))
+    as1 == as2
+  }
+
   lazy val genHeap: Gen[H] = for {
-    isEmpty <- arbitrary[Boolean]
-    h <- if (isEmpty) value(empty) else nonEmptyHeaps
+    // give ourselves a 10% chance of creating an empty heap
+    h <- frequency((1, value(empty)), (10, nonEmptyHeaps))
   } yield h
 
   lazy val nonEmptyHeaps: Gen[H] = for {
@@ -57,3 +74,4 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
 }
+
