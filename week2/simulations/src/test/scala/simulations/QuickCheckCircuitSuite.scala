bfc20233afabb7784a91293b7ccad80f17eb68a2
Chris Price <chris@digitalnotions.com>
2013-11-14 20:21:10 -0800

    More robust demux ScalaCheck tests

--- week2/simulations/src/test/scala/simulations/QuickCheckCircuitSuite.scala
+++ week2/simulations/src/test/scala/simulations/QuickCheckCircuitSuite.scala
@@ -7,6 +7,7 @@ import org.scalatest.prop.Checkers
 import org.scalacheck._
 import Prop._
 import Gen._
+import Arbitrary._
 
 class QuickCheckDemux extends Properties("CircuitSimulator") {
 
@@ -17,14 +18,12 @@ class QuickCheckDemux extends Properties("CircuitSimulator") {
   }
 
   // TODO: we're limiting numCs to 10 since we have to create
-  // 2^numCs Wires for the outputs.  This means that there are
-  // really only 10 possible circuits to generate here, which
-  // in turn means that it's pointless to run the tests more
-  // than 10 times... but I'm a n00b and don't know how to
-  // get ScalaCheck/Generators to work around this, and it
-  // doesn't seem worth the effort atm.
+  // 2^numCs Wires for the outputs.
+
+  val MAX_CONTROL_WIRES = 10
+
   lazy val genDemuxCircuit: Gen[DemuxCircuit] = for {
-    numCs <- choose[Int](0, 10)
+    numCs <- choose[Int](0, MAX_CONTROL_WIRES)
   } yield {
     val in = new Wire
     val cs = List.fill(numCs)(new Wire)
@@ -42,16 +41,67 @@ class QuickCheckDemux extends Properties("CircuitSimulator") {
 
 
   property("all outputs are false if input is false") =
-    forAll { d: DemuxCircuit =>
-      val DemuxCircuit(i, cs, os, circuit) = d
+    forAll(genDemuxCircuit, Gen.listOfN(MAX_CONTROL_WIRES, arbitrary[Boolean])) {
+      (d: DemuxCircuit, cVals: List[Boolean]) =>
+        val DemuxCircuit(i, cs, os, circuit) = d
 
-      i.setSignal(false)
-      circuit.run
-      os.map((w: Wire) => w.getSignal).forall(_ == false)
+        i.setSignal(false)
+        Range(0, cs.length).map((i) => {cs(i).setSignal(cVals(i))})
+        circuit.run
+        os.forall(_.getSignal == false)
+    }
+
+  property("exactly one output is true if input is true") =
+    forAll(genDemuxCircuit, Gen.listOfN(MAX_CONTROL_WIRES, arbitrary[Boolean])) {
+      (d: DemuxCircuit, cVals: List[Boolean]) =>
+        val DemuxCircuit(i, cs, os, circuit) = d
+
+        i.setSignal(true)
+        Range(0, cs.length).map((i) => cs(i).setSignal(cVals(i)))
+        circuit.run
+        os.filter(_.getSignal == true).length == 1
+    }
+
+  property("if input is true, the correct output is true based on the control wires") =
+    forAll(genDemuxCircuit,
+            Gen.listOfN(MAX_CONTROL_WIRES, arbitrary[Boolean]),
+            Gen.listOfN(MAX_CONTROL_WIRES, arbitrary[Boolean])) {
+      (d: DemuxCircuit, initCVals: List[Boolean], nextCVals: List[Boolean]) =>
+        val DemuxCircuit(i, cs, os, circuit) = d
+        val revOs = os.reverse
+
+        def hotO(controlWires: List[Wire]) = {
+          cs.map(_.getSignal).
+            map(if (_) 1 else 0).
+            foldLeft(0)((sum, c) => (2 * sum) + c)
+        }
+        def expectedOutput(i:Int, expectedHot:Int) = {
+          if (i == expectedHot) true else false
+        }
+        def isExpectedOutput(i:Int, expectedHot:Int) = {
+//          println(s"Checking wire $i; expected hot wire: $expectedHot; this wire: ${revOs(i).getSignal}")
+          revOs(i).getSignal == expectedOutput(i, expectedHot)
+        }
+
+        i.setSignal(true)
+        Range(0, cs.length).map((i) => cs(i).setSignal(initCVals(i)))
+        circuit.run
+        val expectedHot = hotO(cs)
+        val firstHotCheck = Range(0, revOs.length).forall(isExpectedOutput(_, expectedHot))
+
+        Range(0, cs.length).map((i) => cs(i).setSignal(nextCVals(i)))
+        circuit.run
+        val expectedHot2 = hotO(cs)
+        val secondHotCheck = Range(0, revOs.length).forall(isExpectedOutput(_, expectedHot2))
+
+        i.setSignal(false)
+        circuit.run
+        val coolCheck = os.forall(_.getSignal == false)
+
+        firstHotCheck & secondHotCheck & coolCheck
     }
 
 }
-
 
 @RunWith(classOf[JUnitRunner])
 class QuickCheckCircuitSuite
