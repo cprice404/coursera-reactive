c378cedf80398cc99ff3391f1524a2865c528b1f
Chris Price <chris@digitalnotions.com>
2013-11-29 18:13:26 -0800

    concatRecovered tests pass

--- week4/suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
+++ week4/suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -37,7 +37,7 @@ trait WikipediaApi {
      *
      * E.g. `"erik", "erik meijer", "martin` should become `"erik", "erik_meijer", "martin"`
      */
-    def sanitized: Observable[String] = ???
+    def sanitized: Observable[String] = obs.map(s => s.replace(" ", "_"))
 
   }
 
@@ -48,7 +48,15 @@ trait WikipediaApi {
      *
      * E.g. `1, 2, 3, !Exception!` should become `Success(1), Success(2), Success(3), Failure(Exception), !TerminateStream!`
      */
-    def recovered: Observable[Try[T]] = ???
+    def recovered: Observable[Try[T]] = {
+      Observable(o => {
+        obs.subscribe(
+          next => o onNext(Success(next)),
+          error => o onNext(Failure(error)),
+          () => o onCompleted()
+        )
+      })
+    }
 
     /** Emits the events from the `obs` observable, until `totalSec` seconds have elapsed.
      *
@@ -56,7 +64,13 @@ trait WikipediaApi {
      *
      * Note: uses the existing combinators on observables.
      */
-    def timedOut(totalSec: Long): Observable[T] = ???
+    def timedOut(totalSec: Long): Observable[T] = {
+      obs.takeUntil(Observable(
+        Future {
+          Await.ready(Promise().future, totalSec seconds)
+          Unit
+        }))
+    }
 
 
     /** Given a stream of events `obs` and a method `requestMethod` to map a request `T` into
@@ -84,7 +98,9 @@ trait WikipediaApi {
      *
      * Observable(Success(1), Succeess(1), Succeess(1), Succeess(2), Succeess(2), Succeess(2), Succeess(3), Succeess(3), Succeess(3))
      */
-    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = ???
+    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = {
+      obs.map(requestMethod(_)).concat.recovered
+    }
 
   }
 
